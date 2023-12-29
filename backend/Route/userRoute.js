96bf7f675f299720836ad305ab1102584cef88eb
Manikant Kumar <123896779+Manikantkr-1004@users.noreply.github.com>
2023-12-29 12:38:35 +0530

    added blacklisting and expiry in token

--- backend/Route/userRoute.js
+++ backend/Route/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 const { userModel } = require("../Model/userModel");
+const { blacklistModel } = require("../Model/blacklistModel");
 require('dotenv').config();
 
 const userRouter = express.Router();
@@ -39,7 +40,7 @@ userRouter.post('/login',async(req,res)=>{
         if(userCheck){
             bcrypt.compare(password,userCheck.password,async(err,result)=>{
                 if(result){
-                    let token = jwt.sign({_id:userCheck._id},process.env.SECRET);
+                    let token = jwt.sign({_id:userCheck._id},process.env.SECRET,{expiresIn:'1h'});
                     res.send({msg:"Login Successfull!!",token,name:userCheck.name,username:userCheck.username});
                 }else{
                     res.status(400).send({msg:"Wrong Credentials!!"});
@@ -48,6 +49,16 @@ userRouter.post('/login',async(req,res)=>{
         }else{
             res.status(400).send({msg:'Email is not Registered!!'});
         }
+    } catch (error) {
+        res.status(500).send({msg:'Internal Server Error!!'})
+    }
+})
+
+userRouter.post('/logout',async(req,res)=>{
+    try {
+        let token = new blacklistModel({token:req.body.token})
+        await token.save();
+        res.status(200).send({msg:"Logout Successfull."})
     } catch (error) {
         res.status(500).send({msg:'Internal Server Error!!'})
     }
